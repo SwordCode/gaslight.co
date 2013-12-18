f748a7a8b47b8291671d79248f576ed664b396e3
Chris Moore <cdmwebs@gmail.com>
2013-12-18 15:48:48 -0500

    Better admin chapter view

--- app/admin/chapter.rb
+++ app/admin/chapter.rb
@@ -1,10 +1,30 @@
 ActiveAdmin.register TrainingApp::Chapter do
+  config.sort_order = "sort_order_asc"
+
   menu label: 'Chapters', parent: 'Training'
 
   controller do
     def permitted_params
       params.permit!
     end
+  end
+
+  index do
+    column(:id) { |_| link_to(_.id, url_for([:edit, :admin, :training_app, _])) }
+    column :course
+    column :title
+    column :sort_order
+    column(:description) { |_| truncate(_.description, length: 40) }
+    column :demo
+    column(:duration) do |c|
+      mm, ss = c.duration.to_i.divmod(60)
+      hh, mm = mm.divmod(60)
+      hours = hh.to_s.ljust(2, '0')
+      minutes = mm.to_s.ljust(2, '0')
+      seconds = ss.to_s.ljust(2, '0')
+      [hours, minutes, seconds].join(':')
+    end
+    default_actions
   end
 
   form do |f|
