ceb90e90876ea6549805650d2f51c69f40f515c7
Chris Moore <cdmwebs@gmail.com>
2013-12-23 16:11:01 -0500

    Don't show top level courses as active

--- engines/training_app/app/controllers/training_app/courses_controller.rb
+++ engines/training_app/app/controllers/training_app/courses_controller.rb
@@ -3,7 +3,7 @@ module TrainingApp
 
     respond_to :html
 
-    expose(:upcoming_courses) { Course.active }
+    expose(:upcoming_courses) { Course.active.where('parent_course_id is not null OR online = true') }
     expose(:previous_courses) { Course.past.limit(4) }
     expose(:course)
   end
