e2acd6e6f2166c01c4174f4cf93075047dd12a44
Chris Moore <cdmwebs@gmail.com>
2014-01-20 22:47:22 -0500

    Better registration view in ActiveAdmin

--- app/admin/registration.rb
+++ app/admin/registration.rb
@@ -1,6 +1,17 @@
 ActiveAdmin.register TrainingApp::Registration do
   menu label: 'Registrations', parent: 'Training'
 
+  index do
+    column :id
+    column :course
+    column :name
+    column :organization
+    column(:amount) { |_| number_to_currency(_.amount) }
+    column(:discounted) { |_| _.discount_code.present? ? "Yes (#{_.discount_code.code})" : 'No' }
+    column :created_at
+    actions
+  end
+
   controller do
     def permitted_params
       params.permit!
