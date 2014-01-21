0f6b388779a3b180ffd3b4a3c263344b23580276
Chris Moore <cdmwebs@gmail.com>
2014-01-20 23:26:53 -0500

    Better admin view of discount code

--- app/admin/discount_code.rb
+++ app/admin/discount_code.rb
@@ -1,6 +1,18 @@
 ActiveAdmin.register TrainingApp::DiscountCode do
   menu parent: 'Training', label: 'Discount Codes'
 
+  index do
+    column :id
+    column :course
+    column :code
+    column :maximum_uses
+    column(:uses) { |_| link_to(_.uses, polymorphic_path([:admin, :training_app, :registrations], q: { discount_code_id_eq: _.id })) } 
+    column :expires_on
+    column :expired?
+    column :created_at
+    actions
+  end
+
   controller do
     def permitted_params
       params.permit!
