d7f2e3187fbb083b227a4822c74009157e0c72e1
Chris Moore <cdmwebs@gmail.com>
2014-02-01 17:11:00 -0500

    Clean up API post controller

--- engines/blog_app/app/controllers/blog_app/api/posts_controller.rb
+++ engines/blog_app/app/controllers/blog_app/api/posts_controller.rb
@@ -7,26 +7,16 @@ module BlogApp
 
       before_filter :old_post?, only: :show
 
-      expose(:posts)        { Post.published.by_publish_date                       }
-      expose(:post)         { Post.slugged(params[:id] || params[:slug])           }
-      expose(:popular_tags) { Post.tag_counts.order('count desc').limit(20)        }
-      expose(:authors)      { Post.authors                                         }
-      expose(:author)       { Author.find_by_tumblr(params[:author].to_s.downcase) }
+      expose(:posts)        { Post.published.by_publish_date                                             }
+      expose(:post)         { Post.find_by_id(params[:id]) || Post.slugged(params[:id] || params[:slug]) }
 
       def index
-        self.posts = self.posts.written_by(author[:tumblr]) if author
-        self.posts = self.posts.tagged_with([params[:tagged]]) if params[:tagged]
         self.posts = self.posts.page(params[:page]).per(items_per_page)
         respond_with posts
       end
 
       def show
-        redirect_to posts_path and return if post.nil?
         respond_with post
-      end
-
-      def podcast_iframe
-        respond_with post, layout: false
       end
 
       def update
@@ -39,11 +29,6 @@ module BlogApp
       def items_per_page
         return Post.count if %w(rss json).include?(request.format)
         index? ? 3 : 20
-      end
-
-      def index?
-        actions = %w(q tagged author year)
-        (params.keys & actions).none?
       end
 
       def old_post?
