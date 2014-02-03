59dbea7017594e28218b4409e27a81672d8683c2
Chris Moore <cdmwebs@gmail.com>
2014-02-03 14:34:56 -0500

    Make current domain a variable

--- config/initializers/rack_rewrite.rb
+++ config/initializers/rack_rewrite.rb
@@ -2,25 +2,27 @@ require 'rack/rewrite'
 
 Gaslight::Application.configure do
   config.middleware.insert_before(Rack::Runtime, Rack::Rewrite) do
-    r301 %r{.*}, 'http://gaslight.co/coffee', if: Proc.new { |rack_env|
+    current_domain = 'gaslight.co'
+
+    r301 %r{.*}, "http://#{current_domain}/coffee", if: Proc.new { |rack_env|
       rack_env['SERVER_NAME'] == 'coffee.gaslightsoftware.com'
     }
 
-    r301 %r{.*}, 'http://training.gaslight.co$&', if: Proc.new { |rack_env|
+    r301 %r{.*}, "http://training.#{current_domain}$&", if: Proc.new { |rack_env|
       rack_env['SERVER_NAME'] == 'training.gaslightsoftware.com'
     }
 
-    r301 %r{.*}, 'http://gaslight.co$&', scheme: 'http', if: Proc.new { |rack_env|
+    r301 %r{.*}, "http://#{current_domain}$&", scheme: 'http', if: Proc.new { |rack_env|
       rack_env['rack.url_scheme'] == 'http' && rack_env['SERVER_NAME'] =~ /^(www.)?gaslightsoftware.com|www.gaslight.co/
     }
 
-    r301 %r{.*}, 'https://gaslight.co$&', scheme: 'https', if: Proc.new { |rack_env|
+    r301 %r{.*}, "https://#{current_domain}$&", scheme: 'https', if: Proc.new { |rack_env|
       rack_env['rack.url_scheme'] == 'https' && rack_env['SERVER_NAME'] =~ /^(www.)?gaslightsoftware.com|www.gaslight.co/
     }
 
     blog_domains = %w[ blog.gaslightsoftware.com blog.gaslight.co ]
 
-    r301 %r{.*}, 'http://gaslight.co/blog', if: Proc.new { |rack_env|
+    r301 %r{.*}, "http://#{current_domain}/blog", if: Proc.new { |rack_env|
       blog_domains.include?(rack_env['SERVER_NAME']) &&
         (rack_env['REQUEST_URI'] == '/' || rack_env['REQUEST_URI'].blank?)
     }
