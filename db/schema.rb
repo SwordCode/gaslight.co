2abfcd2cd1c9c82c00e768445d640eaaae06d0db
Chris Nelson <chris@gaslightsoftware.com>
2014-02-09 16:05:20 -0500

    Adding a map to the course page

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20131212202421) do
+ActiveRecord::Schema.define(version: 20140208192912) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -177,6 +177,8 @@ ActiveRecord::Schema.define(version: 20131212202421) do
     t.datetime "created_at"
     t.datetime "updated_at"
     t.string   "city"
+    t.float    "latitude"
+    t.string   "longitude"
   end
 
 end
