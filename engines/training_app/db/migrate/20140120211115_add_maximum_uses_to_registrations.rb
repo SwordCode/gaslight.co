59c9a5b3e1d1189d502702ff747c0478d684d080
Chris Moore <chrism@gaslight.co>
2014-03-12 22:58:39 -0400

    Remove duplicate migration

--- engines/training_app/db/migrate/20140120211115_add_maximum_uses_to_registrations.rb
+++ engines/training_app/db/migrate/20140120211115_add_maximum_uses_to_registrations.rb
@@ -2,6 +2,5 @@ class AddMaximumUsesToRegistrations < ActiveRecord::Migration
   def change
     add_column :training_app_discount_codes, :maximum_uses, :integer, default: 0
     add_column :training_app_discount_codes, :expires_on, :date
-    add_column :training_app_registrations, :discount_code_id, :integer
   end
 end
